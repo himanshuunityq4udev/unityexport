41ced9742ab42bfb33b7d04470700b4ff27e631d
himanshuunityq4udev <himanshukumar@quantum4u.in>
2024-09-24 17:26:45 +0530

    10
    Loding UI Done
    Music Done

--- Assets/Scripts/Audio/SoundManager.cs
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -6,37 +6,31 @@ using UnityEngine.UI;
 public enum SoundType
 {
     Background,
-    
+
 }
 [RequireComponent(typeof(AudioSource))]
-public class SoundManager : MonoBehaviour,ISoundManager
+public class SoundManager : MonoBehaviour, ISoundManager
 {
-    [SerializeField]private AudioMixer audioMixer;
+    [SerializeField] private AudioMixer audioMixer;
 
-    [SerializeField]  private AudioClip EntryClip;
-     [SerializeField] private AudioClip ExitClip;
+    private AudioClip EntryClip;
+    private AudioClip ExitClip;
+    private AudioClip musicClip;
 
-     [SerializeField] private AudioSource sfxaudioSource;
-     [SerializeField] private AudioSource musicAudioSource;
-     [SerializeField] private Coroutine AudioCoroutine;
+    [SerializeField] private AudioSource sfxaudioSource;
+    [SerializeField] private AudioSource musicAudioSource;
+    [SerializeField] private Coroutine AudioCoroutine;
 
-     [SerializeField] private Slider musicSlider;
-     [SerializeField] private Slider sfxSlider;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
 
     Toggle vibration;
 
     private void Awake()
     {
-        audioMixer = AudioProvider.Instance.AudioMixer;
-        sfxaudioSource = AudioProvider.Instance.SfxSource;
-        musicAudioSource = AudioProvider.Instance.MusicSource;
         EntryClip = AudioProvider.Instance.SoundLibraries[0].audioClips;
         ExitClip = AudioProvider.Instance.SoundLibraries[1].audioClips;
-
-        musicSlider = AudioProvider.Instance.MusicSlider;
-        sfxSlider = AudioProvider.Instance.SfxSlider;
-
-        vibration = AudioProvider.Instance.Vibration;
+        musicClip = AudioProvider.Instance.MusicLibraries[0].audioClip;
 
         sfxaudioSource.playOnAwake = false;
         sfxaudioSource.loop = false;
@@ -45,6 +39,7 @@ public class SoundManager : MonoBehaviour,ISoundManager
 
         if (!PlayerPrefs.HasKey("musicVolume"))
         {
+            Debug.Log("NO Data");
             PlayerPrefs.SetFloat("musicVolume", 1);
             PlayerPrefs.SetFloat("sfxVolume", 1);
             PlayerPrefs.SetInt("Vibration", 1);
@@ -52,29 +47,37 @@ public class SoundManager : MonoBehaviour,ISoundManager
     }
     private void Start()
     {
-       
-
         if (PlayerPrefs.HasKey("musicVolume"))
         {
+            Debug.Log("DiscardVolume");
             DiscardVolume();
         }
         else
         {
+            Debug.Log("SetMusicVolume and SetSFXVolume");
             SetMusicVolume();
             SetSFXVolume();
         }
 
-        if (PlayerPrefs.HasKey("Vibration"))
-        {
-            if (PlayerPrefs.GetInt("Vibration") == 0)
-            {
-                vibration.isOn = false;
-            }
-            else
-            {
-                vibration.isOn = true;
-            }
-        }
+        musicAudioSource.clip = musicClip;
+        musicAudioSource.Play();
+
+
+        // Add listeners to the sliders
+        musicSlider.onValueChanged.AddListener(delegate { SetMusicVolume(); });
+        sfxSlider.onValueChanged.AddListener(delegate { SetSFXVolume(); });
+
+        /* if (PlayerPrefs.HasKey("Vibration"))
+         {
+             if (PlayerPrefs.GetInt("Vibration") == 0)
+             {
+                 vibration.isOn = false;
+             }
+             else
+             {
+                 vibration.isOn = true;
+             }
+         }*/
     }
 
     public void EnterPlayClip(bool PlayAudio)
@@ -129,12 +132,14 @@ public class SoundManager : MonoBehaviour,ISoundManager
 
     public void SetMusicVolume()
     {
+        Debug.Log("SetMusicVolume");
         float volume = musicSlider.value;
         audioMixer.SetFloat("music", Mathf.Log10(volume) * 20);
         //PlayerPrefs.SetFloat("musicVolume", volume);
     }
     public void SetSFXVolume()
     {
+        Debug.Log("SetSFXVolume");
         float sfxVolume = sfxSlider.value;
         audioMixer.SetFloat("sfx", Mathf.Log10(sfxVolume) * 20);
         //PlayerPrefs.SetFloat("sfxVolume", sfxVolume);
@@ -142,6 +147,7 @@ public class SoundManager : MonoBehaviour,ISoundManager
 
     public void SaveVolume()
     {
+        Debug.Log("SaveVolume");
         float volume = musicSlider.value;
         audioMixer.SetFloat("music", Mathf.Log10(volume) * 20);
         PlayerPrefs.SetFloat("musicVolume", volume);
@@ -153,6 +159,7 @@ public class SoundManager : MonoBehaviour,ISoundManager
 
     public void DiscardVolume()
     {
+        Debug.Log("DiscardVolume");
         musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
         sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume");
         SetMusicVolume();
@@ -160,28 +167,28 @@ public class SoundManager : MonoBehaviour,ISoundManager
     }
 
 
-   /* public AudioClip GetAudioClipByName(string _audioName)
-    {
-        foreach (var soundLibrary in SoundLibraries)
-        {
-            if (soundLibrary.audioName == _audioName)
-            {
-                return soundLibrary.audioClips;
-            }
-        }
-        return null;
-    }*/
-   /* public AudioClip GetMusicTrackByName(string _trackName)
-    {
-        foreach (var track in musicLibraries)
-        {
-            if (track.trackName == _trackName)
-            {
-                return track.audioClip;
-            }
-        }
-        return null;
-    }*/
+    /* public AudioClip GetAudioClipByName(string _audioName)
+     {
+         foreach (var soundLibrary in SoundLibraries)
+         {
+             if (soundLibrary.audioName == _audioName)
+             {
+                 return soundLibrary.audioClips;
+             }
+         }
+         return null;
+     }*/
+    /* public AudioClip GetMusicTrackByName(string _trackName)
+     {
+         foreach (var track in musicLibraries)
+         {
+             if (track.trackName == _trackName)
+             {
+                 return track.audioClip;
+             }
+         }
+         return null;
+     }*/
 
 
     public void DiscardVibration()
@@ -213,7 +220,7 @@ public class SoundManager : MonoBehaviour,ISoundManager
     public void Play2Dsound(string _audioName)
     {
 
-       // sfxSource.PlayOneShot(GetAudioClipByName(_audioName));
+        // sfxSource.PlayOneShot(GetAudioClipByName(_audioName));
         if (vibration.isOn)
         {
             // HapticFeedback.MediumFeedback();
